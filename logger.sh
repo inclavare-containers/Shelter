8abfcdaccc9a2f0778fe8a7bd8c70ba0f1db42e0
Jia Zhang <zhang.jia@linux.alibaba.com>
2024-07-08 11:18:36 +0800

    logger: support color display for log levels
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- logger.sh
+++ logger.sh
@@ -9,14 +9,39 @@ LOG_LEVEL=${LOG_LEVEL:-$INFO}
 
 log() {
     local log_level_int=$1
+
+    if [[ $log_level_int -gt $LOG_LEVEL ]]; then
+        return
+    fi
+
+    case $log_level_int in
+        $ERROR)
+            # Red
+            color="31"
+            ;;
+        $WARN)
+            # Yellow
+            color="33"
+            ;;
+        $DEBUG)
+            # Cyan
+            color="36"
+            ;;
+        $INFO)
+            # Green
+            color="32"
+            ;;
+        *)
+            # Use default color for unrecognized log level
+            color="38"
+            ;;
+    esac
+
     local log_level_str=$2
     local message=${@:3}
     local timestamp=$(date +"%Y-%m-%d %H:%M:%S")
     local log_message="[$timestamp] [$log_level_str] - $message"
-
-    if [[ $log_level_int -le $LOG_LEVEL ]]; then
-        echo $log_message
-    fi
+    echo -e "\033[1;${color}m${log_message}\033[0m"
 }
 
 error() {
