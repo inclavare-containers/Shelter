dfc3c53db58964514aa1e8c00301439de48c52b8
Jia Zhang <zhang.jia@linux.alibaba.com>
2024-12-10 20:11:55 +0800

    shelter+kbs.sh: improve the code readability
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- demos/verify-signature/kbs.sh
+++ demos/verify-signature/kbs.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 
-## stop kbs if it is active and return immediately
+# Stop kbs if launched already
 systemctl --user -q is-active kbs.service && \
-systemctl --user stop kbs && \
-echo "stop kbs service" && \
-exit 0
+    systemctl --user stop kbs
 
-## start kbs if it isn't active
+# Start kbs if it isn't launched
 if [ -d "/tmp/kbs" ]; then
-    rm -r /tmp/kbs
+    rm -rf /tmp/kbs
 fi
 
 mkdir /tmp/kbs
 mkdir /tmp/kbs/{repository,attestation-service}
-cp demos/verify-signature/kbs/* /tmp/kbs/
+cp demos/verify-signature/kbs/* /tmp/kbs
 
 openssl genpkey -algorithm ed25519 > /tmp/kbs/private.key
 openssl pkey -in /tmp/kbs/private.key -pubout -out /tmp/kbs/public.pub
 
-systemd-run --user --description="kbs test server" --unit="kbs" -G /usr/local/libexec/shelter/kbs -c /tmp/kbs/config.toml && \
-echo "start kbs"
+systemd-run --user --description="KBS Testing Server" --unit="kbs" \
+  -G /usr/local/libexec/shelter/kbs -c /tmp/kbs/config.toml && \
+    echo "KBS service started"
 
 sed '/kern_cmdline =/ s/"$/ KBS_URL=http:\/\/10.0.2.2:8080 PASSPHRASE_PATH=default\/shelter-demo\/passphrase"/' /etc/shelter.conf > /tmp/kbs/shelter.conf
 
@@ -27,9 +26,10 @@ if [ "$(toml get --toml-path /var/lib/shelter/images/shelter-demos/image_info.to
     sleep 5
 
     /usr/local/libexec/shelter/kbs-client \
-    config --auth-private-key /tmp/kbs/private.key \
-    set-resource --resource-file /var/lib/shelter/images/shelter-demos/passphrase \
-    --path default/shelter-demos/passphrase
+      config --auth-private-key /tmp/kbs/private.key \
+      set-resource \
+        --resource-file /var/lib/shelter/images/shelter-demos/passphrase \
+        --path default/shelter-demos/passphrase
 
-    echo "upload passphrase"
+    echo "Passphrase configured for disk image decryption"
 fi
