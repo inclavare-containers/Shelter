18dbda9cb7eab1607b5d54fe2fcd604aec69ae9d
Jia Zhang <zhang.jia@linux.alibaba.com>
2024-12-24 13:17:03 +0800

    kbs/shelter/build-shelter-kbs.sh: restructure
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- kbs/shelter/build-shelter-kbs.sh
+++ kbs/shelter/build-shelter-kbs.sh
@@ -2,16 +2,13 @@
 
 KBS_DIR="${KBS_DIR:-/usr/local/libexec/shelter/kbs}"
 KBS_PORT="${KBS_PORT:-6773}"
-SALT="${SALT:-shelter-local-kbs-salt}"
+NAME="${NAME:=shelter-kbs}"
+SALT="${SALT:-shelter-shelter-kbs-salt}"
 ITER=${ITER:-1000}
+PASSPHRASE="${PASSPHRASE:-}"
+PASSPHRASE_PATH="${PASSPHRASE_PATH:-default/shelter/passphrase}"
 
-PASSPHRASE="$1"
-if [ -z "${PASSPHRASE}" ]; then
-    echo "Please specify the content of passphrase in hex string as the parameter"
-    exit 1
-fi
-
-d="$(mktemp -d /tmp/build-shelter-local-kbs-XXXXXX)"
+d="$(mktemp -d /tmp/build-shelter-kbs-XXXXXX)"
 
 trap_handler() {
     trap - SIGINT SIGTERM EXIT ERR
@@ -34,50 +31,53 @@ trap 'trap_handler $LINENO $?' SIGINT SIGTERM EXIT ERR
 # Step 1: build and run cbmkpasswd to generate the IKM based on
 # shelter itself
 
-cat >"$d/build-cbmkpasswd.conf" <<EOF
+if [ -n "${PASSPHRASE}" ]; then
+    cat >"$d/build-cbmkpasswd.conf" <<EOF
 binary=(/usr/local/libexec/shelter/cbmkpasswd:/sbin/cbmkpasswd)
 EOF
 
-shelter build \
-  -t cbmkpasswd \
-  -T initrd \
-  -c "$d/build-cbmkpasswd.conf" || {
-    echo "Failed to build cbmkpasswd image"
-    exit 1
-  }
+    shelter build \
+      -t cbmkpasswd \
+      -T initrd \
+      -c "$d/build-cbmkpasswd.conf" || {
+        echo "Failed to build cbmkpasswd image"
+        exit 1
+      }
 
-# KBS running in shelter doesn't require to connect another KBS
-conf=/etc/shelter.conf
-if grep -q KBS_URL= "${conf}" 2>/dev/null; then
-    sed "/kern_cmdline =/ s|KBS_URL=http://[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:[0-9]\{1,5\}\+||" \
-      "${conf}" > "$d/shelter.conf"
-    conf="$d/shelter.conf"
+    # KBS running in shelter doesn't require to connect another KBS
+    conf=/etc/shelter.conf
+    if grep -q KBS_URL= "${conf}" 2>/dev/null; then
+        sed "/kern_cmdline =/ s|KBS_URL=http://[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:[0-9]\{1,5\}\+||" \
+          "${conf}" > "$d/shelter.conf"
+        conf="$d/shelter.conf"
+    fi
+
+    # Generate IKM
+    ikm="$(LOG_LEVEL=0 shelter run \
+      -c "${conf}" \
+      cbmkpasswd -- \
+      "cbmkpasswd --salt "${SALT}" --iter "${ITER}" -n 256")"
+    if [ $? -ne 0 ]; then
+        echo "Failed to run cbmkpasswd image"
+        exit 1
+    fi
+    if [ $(echo -ne "${ikm}" | wc -c) -ne 512 ]; then
+        echo "Invalid length of ikm"
+        exit 1
+    fi
+
+    # Step 2: generate encrypted passphrase bundle
+
+    encp="$(${KBS_DIR}/shelter/encp-encoder "${PASSPHRASE}" "${ikm}")"
+    if [ $? -ne 0 ]; then
+        echo "Failed to generate encrypted passphrase bundle"
+        exit 1
+    fi
+
+    echo -ne "${encp}" >"$d/passphrase.encp"
 fi
 
-ikm="$(LOG_LEVEL=0 shelter run \
-  -c "${conf}" \
-  cbmkpasswd -- \
-  "cbmkpasswd --salt "${SALT}" --iter "${ITER}" -n 256")"
-if [ $? -ne 0 ]; then
-    echo "Failed to run cbmkpasswd image"
-    exit 1
-fi
-if [ $(echo -ne "${ikm}" | wc -c) -ne 512 ]; then
-    echo "Invalid length of ikm"
-    exit 1
-fi
-
-# Step 3: generate encrypted passphrase bundle
-
-encp="$(${KBS_DIR}/shelter/encp-encoder "${PASSPHRASE}" "${ikm}")"
-if [ $? -ne 0 ]; then
-    echo "Failed to generate encrypted passphrase bundle"
-    exit 1
-fi
-
-echo -ne "${encp}" >"$d/passphrase.encp"
-
-# Step 4: build KBS image
+# Step 3: build KBS image
 
 openssl genpkey -algorithm ed25519 >"$d/private.key"
 openssl pkey -in "$d/private.key" -pubout -out "$d/public.pub"
@@ -114,17 +114,28 @@ EOF
 # Note that the passphrase is provisioned during the build, instead of
 # using kbs-client to register the passphrase as a resource during the
 # runtime.
-cat >"$d/build-local-kbs.conf" <<EOF
+if [ -n "${PASSPHRASE}" ]; then
+    cat >"$d/build-shelter-kbs.conf" <<EOF
 binary=(/usr/local/libexec/shelter/kbs/kbs:/sbin/kbs)
-file=($d/config.toml:/kbs/config.toml ${KBS_DIR}/policy.rego:/kbs/policy.rego $d/public.pub:/kbs/public.pub $d/passphrase.encp:/kbs/repository/default/shelter/passphrase)
+file=($d/config.toml:/kbs/config.toml ${KBS_DIR}/policy.rego:/kbs/policy.rego $d/public.pub:/kbs/public.pub $d/passphrase.encp:/kbs/repository/${PASSPHRASE_PATH})
 EOF
+else
+    cat >"$d/build-shelter-kbs.conf" <<EOF
+binary=(/usr/local/libexec/shelter/kbs/kbs:/sbin/kbs)
+file=($d/config.toml:/kbs/config.toml ${KBS_DIR}/policy.rego:/kbs/policy.rego $d/public.pub:/kbs/public.pub)
+EOF
+fi
 
 shelter build \
-  -t local-kbs \
-  -c "$d/build-local-kbs.conf" \
+  -t "${NAME}" \
+  -c "$d/build-shelter-kbs.conf" \
   -T initrd || {
-    echo "Failed to build local-kbs image"
+    echo "Failed to build ${NAME} image"
     exit 1
   }
 
-echo "IKM: ${ikm}" >&2
+if [ -n "${PASSPHRASE}" ]; then
+    echo "IKM: ${ikm}" >&2
+fi
+
+echo "Private Key: $(cat "$d/private.key" | xxd -p | tr -d "\n")"
