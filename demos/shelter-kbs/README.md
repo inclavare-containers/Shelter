bf4a533165ca30bbe1564569ff6d4a9498d61289
Jia Zhang <zhang.jia@linux.alibaba.com>
2024-12-24 13:18:07 +0800

    demos/shelter-kbs: rewrite
    Signed-off-by: Jia Zhang <zhang.jia@linux.alibaba.com>

--- demos/shelter-kbs/README.md
+++ demos/shelter-kbs/README.md
@@ -1,27 +1,27 @@
-本DEMO展示了如何用Shelter来运行KBS远程证明服务，并展示了如何通过kbs-client向KBS事先注册资源。
+本DEMO展示了如何用Shelter来运行KBS远程证明服务，并展示了如何通过kbs-client向KBS注册资源，并通过KBS下载资源。
 
 当需要把KBS远程服务与要保护的工作负载部署和运行在同一台主机上的时候，就会用到本DEMO所展示的方式来安全可信地运行KBS远程证明服务。
 
 # 执行步骤
 
 ```shell
-./shelter-run-kbs.sh
+./run-shelter-kbs.sh
 ```
 
 # 预期结果
 
 ```
-[2024-12-19 19:06:58][INFO] Succeed to build the image "kbs"
-[2024-12-19 19:06:58][INFO] Using Image ID: kbs
-Running as unit: shelter_kbs.service
-[2024-12-19T11:07:02Z INFO  kbs] Using config file /kbs/config.toml
-[2024-12-19T11:07:02Z WARN  kbs] insecure APIs are enabled
-[2024-12-19T11:07:02Z WARN  attestation_service::rvps] No RVPS address provided and will launch a built-in rvps
-[2024-12-19T11:07:02Z INFO  attestation_service::token::simple] No Token Signer key in config file, create an ephemeral key and without CA pubkey cert
-[2024-12-19T11:07:02Z INFO  kbs] Starting HTTP server at [0.0.0.0:6773]
-[2024-12-19T11:07:02Z INFO  actix_server::builder] starting 2 workers
-[2024-12-19T11:07:02Z INFO  actix_server::server] Tokio runtime found; starting in existing Tokio runtime
-[2024-12-19T11:07:02Z INFO  actix_web::middleware::logger] 10.0.2.2 "POST /kbs/v0/resource/default/shelter/passphrase HTTP/1.1" 200 0 "-" "kbs-client/0.1.0" 0.000222
-Set resource success 
- resource: kLbSC6kMBwxpvFtFR4QMVlrD3uLD5CIOmUmqqARLUS/x/5Zm7pKX7qDhtBEQA9Eh8+Drm2ux1jtJpier0/6Cpt58tLLL1zEBAeC9LKFHPoyoVyn6gu2SSjcCFa2WRK28Y/3k/QJgeRyywebJ3dgj02qXgs3O8ZEvfZi+ujl63T4PGYWeM16yuP2sFh0OiMO7MTwe8Bx4Q2LmLX68fB4mBgXuyWGKq5ePn83NWO7IODXqDVxgBUxb9cPpgC4TxZ41Bdy34c1G77uHUSFuplCeoRjq99BXgzgjYbLnvX8qxcuQd8PMgGDLmNqDibAaraEpDCA+bHEKKRRq4SDM3KKTq1tP584mgKmikK1eMcxtVp02JPYkwBvXswYafXULDtIR4gmQLIutpH2CJo0ectlpwK+B3+aAXYbimd+ffQpt4Srj7uld36GnHwqncj+6S7X9PCketIRDw8UED/VnC6N6ocIqwPAQ691BR1p/UrOucUQYBgXusGpkWPV8l1a6k3U24Vukq8ywFpQp7d9XfYwR97VfbW4ut/bAmM6LQHPIdnAsEe/quaSXCCbmIf6TutiUdNjeRhHDJhjP6C3J6OjApjVcTjWP4uDqjg7krbp5mkGnR1Ua7NGsN2XgrA0ZLf3dLiYkNEX9XrCsb157LjPUIhoVpImXpNWE9O5u8yG1NJT8XAlm4V7r5bGzJMDwnrCIvRBzJiPOgojX78MyH4A1IHCTiUEa4D+I7uKeTlLxCEpDhJjq5FOoFH+5cWb7POOX1Tz8feVD/OCjSam1A5OpMNb2aiyQ22Phjcwv9ygV9gOo7aOTFEPUoJZUy3NI+ToDahNPq60mvxwnV4eFrpcDBsmdvFAssBngLxoFOxyGC0i59OprsbWZ6wCYSmH31OwpSUehOjQN+221euQCBd+Q/uAz05ObSa8ymhLY47kltG1SKo1DzU3IkgKjw0eX838uW1L4ckWw3pb14Dq3YYbyHb190mtKIF0GCrgz1MjwQRANqGcOWNgySfCSfFtP4ZGs+3wcB/4ZWP08MZ5mm6Dls6uUSJu87+sJhCbwA5OQlYsLooEU8FnfMCFN8HtEb6SYLM+vjOVzmrrg17rFJO+rc9jzCIYeZ/4puCnEJlJY/DPw1/9XMGxLLZ7dDLd/mx1HuxHUOoeFflSCl/6iEdfafGkmnChfthA5HPkg6ZHxXjhxIUJsS7f8/OSswTUrycKubHW+WMN53zmdiZUoXpQMGej/03XHYwskVgoIvs45weeB3fiHz17yPkRLMlTox4Amig2eHvBXsrxllWC73ceiQcqcNx2xzN17sjuzdhWwNxThS1HDk+y+bq+oET6U4yYPbIVlOzQstyd8bW//bzzwgw==
- ```
+[2024-12-24T05:04:10Z INFO  kbs::http::attest] Auth API called.
+[2024-12-24T05:04:10Z INFO  actix_web::middleware::logger] 10.0.2.2 "POST /kbs/v0/auth HTTP/1.1" 200 74 "-" "attestation-agent-kbs-client/0.1.0" 0.000107
+[2024-12-24T05:04:10Z INFO  kbs::http::attest] Attest API called.
+[2024-12-24T05:04:10Z INFO  attestation_service] Sample Verifier/endorsement check passed.
+[2024-12-24T05:04:10Z INFO  attestation_service] Policy check passed.
+[2024-12-24T05:04:10Z INFO  attestation_service] Attestation Token (Simple) generated.
+[2024-12-24T05:04:10Z INFO  actix_web::middleware::logger] 10.0.2.2 "POST /kbs/v0/attest HTTP/1.1" 200 2171 "-" "attestation-agent-kbs-client/0.1.0" 0.002196
+[2024-12-24T05:04:10Z INFO  kbs::http::resource] Cookie 80cddda728794f6c808aa58f2741667e request to get resource
+[2024-12-24T05:04:10Z INFO  kbs::http::resource] Get resource from kbs:///default/run-shelter-kbs-demo/passphrase
+[2024-12-24T05:04:10Z INFO  kbs::http::resource] Resource access request passes policy check.
+[2024-12-24T05:04:10Z INFO  actix_web::middleware::logger] 10.0.2.2 "GET /kbs/v0/resource/default/run-shelter-kbs-demo/passphrase HTTP/1.1" 200 529 "-" "attestation-agent-kbs-client/0.1.0" 0.000448
+Succeed to retrieve the sample passphrase from shelter-kbs
+[2024-12-24 13:04:11][INFO] Using Image ID: shelter-kbs-demo
+```
--- demos/shelter-kbs/run-shelter-kbs.sh
+++ demos/shelter-kbs/run-shelter-kbs.sh
@@ -3,105 +3,98 @@
 KBS_DIR="${KBS_DIR:-/usr/local/libexec/shelter/kbs}"
 KBS_PORT="${KBS_PORT:-6773}"
 
-d="$(mktemp -d /tmp/run-kbs-XXXXXX)"
+name=shelter-kbs-demo
+passphrase_sample="run-${name}-passphrase"
 
-openssl genpkey -algorithm ed25519 > "$d/private.key"
-openssl pkey -in "$d/private.key" -pubout -out "$d/public.pub"
+d="$(mktemp -d "/tmp/run-${name}-XXXXXX")"
+stop_kbs=0
 
-cat >"$d/policy.rego" <<EOF
-package policy
-default allow = true
-EOF
+trap_handler() {
+    trap - SIGINT SIGTERM EXIT ERR
 
-cat >"$d/config.toml" <<EOF
-insecure_http = true
-insecure_api = true
-sockets = ["0.0.0.0:${KBS_PORT}"]
-auth_public_key = "/kbs/public.pub"
+    local line_no="$1"
+    local err=$2
 
-[attestation_token_config]
-attestation_token_type = "CoCo"
+    if [ ${err} -ne 0 ] && [ "${line_no}" != "1" ]; then
+        echo "Error occurred on line ${line_no}, exit code: ${err}"
+    fi
 
-[repository_config]
-type = "LocalFs"
-dir_path = "/kbs/repository"
+    if [ ${stop_kbs} -eq 1 ]; then
+        shelter stop "${name}"
+    fi
 
-[as_config]
-work_dir = "/kbs/attestation-service"
-policy_engine = "opa"
-attestation_token_broker = "Simple"
+    # TODO: use shred -u to delete
+    rm -rf "$d"
 
-[as_config.attestation_token_config]
-duration_min = 5
+    exit ${err}
+}
 
-[as_config.rvps_config]
-store_type = "LocalFs"
-remote_addr = ""
+trap 'trap_handler $LINENO $?' SIGINT SIGTERM EXIT ERR
 
-[policy_engine_config]
-policy_path = "/kbs/policy.rego"
-EOF
+# Step 1: build the shelter KBS DEMO image
 
-cat >$d/build.conf <<EOF
-binary=(/usr/local/libexec/shelter/kbs/kbs:/sbin/kbs)
-file=($d/config.toml:/kbs/config.toml $d/policy.rego:/kbs/policy.rego $d/public.pub:/kbs/public.pub)
-EOF
-
-shelter build \
-  -t kbs-demo \
-  -c "$d/build.conf" \
-  -T initrd
-
-conf=/etc/shelter.conf
-
-# KBS running in shelter doesn't require to connect another KBS
-if grep -q KBS_URL= "$conf"; then
-    sed "/kern_cmdline =/ s|KBS_URL=http://[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}:[0-9]\{1,5\}\+||" "$conf" > "$d/shelter.conf"
-    conf="$d/shelter.conf"
+priv_key="$(KBS_DIR="${KBS_DIR}" \
+  KBS_PORT="${KBS_PORT}" \
+  NAME="${name}" \
+  SALT="run-${name}-salt" \
+  ITER=1000 \
+  LOG_LEVEL=0 \
+  ${KBS_DIR}/shelter/build-shelter-kbs.sh)"
+if [ $? -ne 0 ]; then
+    exit 1
 fi
 
-shelter run \
-  -c "$conf" \
-  -p ${KBS_PORT}:${KBS_PORT} \
-  kbs-demo \
-    /sbin/kbs -c /kbs/config.toml &
+if [[ "${priv_key}" == Private\ Key:\ * ]]; then
+    priv_key="${priv_key#*Private\ Key:\ }"
+else
+    echo "Failed to retrieve the private key"
+    exit 1
+fi
 
-cat >"$d/passphrase.b64" <<EOF
-kLbSC6kMBwxpvFtFR4QMVlrD3uLD5CIOmUmqqARLUS/x/5Zm7pKX7qDhtBEQA9Eh8+Drm2ux1jtJ
-pier0/6Cpt58tLLL1zEBAeC9LKFHPoyoVyn6gu2SSjcCFa2WRK28Y/3k/QJgeRyywebJ3dgj02qX
-gs3O8ZEvfZi+ujl63T4PGYWeM16yuP2sFh0OiMO7MTwe8Bx4Q2LmLX68fB4mBgXuyWGKq5ePn83N
-WO7IODXqDVxgBUxb9cPpgC4TxZ41Bdy34c1G77uHUSFuplCeoRjq99BXgzgjYbLnvX8qxcuQd8PM
-gGDLmNqDibAaraEpDCA+bHEKKRRq4SDM3KKTq1tP584mgKmikK1eMcxtVp02JPYkwBvXswYafXUL
-DtIR4gmQLIutpH2CJo0ectlpwK+B3+aAXYbimd+ffQpt4Srj7uld36GnHwqncj+6S7X9PCketIRD
-w8UED/VnC6N6ocIqwPAQ691BR1p/UrOucUQYBgXusGpkWPV8l1a6k3U24Vukq8ywFpQp7d9XfYwR
-97VfbW4ut/bAmM6LQHPIdnAsEe/quaSXCCbmIf6TutiUdNjeRhHDJhjP6C3J6OjApjVcTjWP4uDq
-jg7krbp5mkGnR1Ua7NGsN2XgrA0ZLf3dLiYkNEX9XrCsb157LjPUIhoVpImXpNWE9O5u8yG1NJT8
-XAlm4V7r5bGzJMDwnrCIvRBzJiPOgojX78MyH4A1IHCTiUEa4D+I7uKeTlLxCEpDhJjq5FOoFH+5
-cWb7POOX1Tz8feVD/OCjSam1A5OpMNb2aiyQ22Phjcwv9ygV9gOo7aOTFEPUoJZUy3NI+ToDahNP
-q60mvxwnV4eFrpcDBsmdvFAssBngLxoFOxyGC0i59OprsbWZ6wCYSmH31OwpSUehOjQN+221euQC
-Bd+Q/uAz05ObSa8ymhLY47kltG1SKo1DzU3IkgKjw0eX838uW1L4ckWw3pb14Dq3YYbyHb190mtK
-IF0GCrgz1MjwQRANqGcOWNgySfCSfFtP4ZGs+3wcB/4ZWP08MZ5mm6Dls6uUSJu87+sJhCbwA5OQ
-lYsLooEU8FnfMCFN8HtEb6SYLM+vjOVzmrrg17rFJO+rc9jzCIYeZ/4puCnEJlJY/DPw1/9XMGxL
-LZ7dDLd/mx1HuxHUOoeFflSCl/6iEdfafGkmnChfthA5HPkg6ZHxXjhxIUJsS7f8/OSswTUrycKu
-bHW+WMN53zmdiZUoXpQMGej/03XHYwskVgoIvs45weeB3fiHz17yPkRLMlTox4Amig2eHvBXsrxl
-lWC73ceiQcqcNx2xzN17sjuzdhWwNxThS1HDk+y+bq+oET6U4yYPbIVlOzQstyd8bW//bzzwgw==
-EOF
-base64 -d "$d/passphrase.b64" >"$d/passphrase"
-# TODO: use shred -u to delete
-rm -f "$d/passphrase.b64"
-chmod 600 "$d/passphrase"
+# Step 2: run the shelter KBS DEMO image
 
-# Await for KBS service getting ready
-sleep 5
+SHELTER_KBS=1 \
+  KBS_PORT="${KBS_PORT}" \
+  NAME="${name}" \
+  ${KBS_DIR}/start-kbs
 
-${KBS_DIR}/../kbs-client \
-  --url http://127.0.0.1:${KBS_PORT} \
-  config --auth-private-key "$d/private.key" \
-  set-resource \
-    --resource-file "$d/passphrase" \
-    --path default/shelter/passphrase
+stop_kbs=1
 
-shelter stop kbs-demo
+# Step 3: configure and register the local KBS
 
-# TODO: use shred -u to delete
-rm -rf "$d"
+passphrase="$(echo -ne "${passphrase_sample}" | xxd -p | tr -d "\n")"
+
+echo -ne "${priv_key}" | xxd -p -r >"$d/private_key"
+
+KBS_ADDRESS=10.0.2.2 \
+  KBS_PORT="${KBS_PORT}" \
+  KBS_DIR="${KBS_DIR}" \
+  PASSPHRASE="${passphrase}" \
+  PASSPHRASE_PATH="default/run-${name}/passphrase" \
+  PRIVATE_KEY_PATH="$d/private_key" \
+  ${KBS_DIR}/config-kbs >"$d/${name}.conf"
+
+# Step 4: donwload passphrase
+
+passphrase_hex="$(${KBS_DIR}/../kbs-client \
+                    --url "http://127.0.0.1:${KBS_PORT}" \
+                    get-resource \
+                      --path "default/run-${name}/passphrase" | \
+                  base64 -d | xxd -p | tr -d "\n")"
+
+magic="$(echo -ne "${passphrase_hex}" | cut -c 1-8)"
+# "ENCP" magic
+if [ "${magic}" == "454e4350" ]; then
+    encp="$(echo -ne "${passphrase_hex}" | xxd -p -r)"
+    passphrase="$(${KBS_DIR}/encp-decoder "${encp}")"
+else
+    passphrase="${passphrase_hex}"
+fi
+
+if [ "$(echo -ne "${passphrase}" | xxd -p -r)" == "${passphrase_sample}" ]; then
+    echo -e "\033[1;32mSucceed to retrieve the sample passphrase from shelter-kbs\033[0m"
+    exit 0
+else
+    echo -e "\033[1;31mFailed to retrieve the sample passphrase from shelter-kbs\033[0m"
+    exit 1
+fi
